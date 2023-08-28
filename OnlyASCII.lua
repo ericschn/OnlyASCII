d9e4b4ebfd50212659509ed2b7755f283db7c398
ericschn <eschneider327@gmail.com>
2023-08-28 11:37:05 -0500

    initial commit

---
+++ OnlyASCII.lua
@@ -0,0 +1,21 @@
+OnlyASCII_ChatFrame_OnEvent = ChatFrame_OnEvent
+
+function ChatFrame_OnEvent(event)
+  local chatMsg = arg1
+
+  if (event == "CHAT_MSG_CHANNEL" or
+        event == "CHAT_MSG_YELL" or
+        event == "CHAT_MSG_SAY" or
+        event == "CHAT_MSG_GUILD" or
+        event == "CHAT_MSG_WHISPER") and arg2 and arg1 then
+    for i = 1, string.len(chatMsg) do
+      local byte = string.byte(string.sub(chatMsg, i, i))
+      if byte > 126 then
+        -- arg1 = "--BLOCKED-- " .. chatMsg
+        return false
+      end
+    end
+  end
+
+  OnlyASCII_ChatFrame_OnEvent(event)
+end
